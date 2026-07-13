2dcf3a9488067b31d97c840904100050b2430e57
rckwd <key.grunge@gmail.com>
2026-07-13 12:15:09 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,11 @@
-self.addEventListener('fetch', () => {});
+// sw.js
+const CACHE_NAME = 'nota-app-v1';
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => {
+    return cache.addAll(['/index.html', '/css/style.css', '/js/main.js']);
+  }));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+});

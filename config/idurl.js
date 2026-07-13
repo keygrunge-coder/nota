a51dc2e2ce7fc8ae467b15630352c12172cdbcfe
rckwd <key.grunge@gmail.com>
2026-07-13 12:14:22 +0700

    Update idurl.js

--- config/idurl.js
+++ config/idurl.js
@@ -2,5 +2,5 @@
 const URL_CONFIG = {
     "ALDI": "URL_SCRIPT_ALDI_ANDA",
     "LEORA": "URL_SCRIPT_LEORA_ANDA",
-    "NJ": "URL_SCRIPT_NJ_ANDA"
+    "NJ": "https://script.google.com/macros/s/AKfycby7AUYawCKgafTMGcGuuWxwsQbdNLguROdX5FSoVMISSgSx--dV9bparoRFReRb9lxQ/exec"
 };
